b666862d00482fe275508fa5595915803cf398e2
Arno de Greeuw <arnodegreeuw@gmail.com>
2015-11-12 23:33:15 +0100

    add class

---
+++ UsbPortListener/Listener.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsbPortListener
+{
+
+    public delegate void SerialReceived(string data);
+    public class Listener : System.IO.Ports.SerialPort
+    {
+       public SerialReceived OnSerialReceived { get; set; }
+
+        void OnSerialReceivedHandler(string data)
+        {
+            if (OnSerialReceived != null)
+                OnSerialReceived(data);
+
+        }
+
+       public  Listener(string portName, int baudRate) : base(portName, baudRate )
+       {
+
+            this.DataReceived += Listener_DataReceived;
+       }
+
+        private void Listener_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            System.IO.Ports.SerialPort p = sender as System.IO.Ports.SerialPort;
+            OnSerialReceivedHandler(p.ReadExisting());
+        }
+    }
+}
